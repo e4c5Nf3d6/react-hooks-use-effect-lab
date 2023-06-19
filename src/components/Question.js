eddf834f086d1d450ba6dd8b79d1f40d2f981464
Maria Jenkins <macjenkin@gmail.com>
2023-06-19 06:12:54 -0500

    complete lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  function handleTimeRemaining() {
+    if (timeRemaining > 1) {
+      setTimeRemaining(timeRemaining - 1)
+    } else {
+      setTimeRemaining(10)
+      onAnswered(false)
+    }
+  }
+
+  useEffect(() => {
+    const timeoutId = setTimeout(handleTimeRemaining, 1000)
+
+    return function() {
+      clearTimeout(timeoutId)
+    }
+  })
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
